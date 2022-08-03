f1b79c72db822ff458b3c70f3f2652fceee6e1cc
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2022-08-03 01:16:42 -0300

    API Funcionando

--- components/Solicitudes.js
+++ components/Solicitudes.js
@@ -1,14 +1,35 @@
-import {useEffect, useState} from 'react';
+/*import {useEffect, useState} from 'react';
 import axios from "axios";
 import axiosFetch from '../config/axios';
 
 
 
-useEffect(() => {
-    const fetchSol = async () => {
+useEffect( () => {
+    const getSol = async () => {
       const result = await axiosFetch("/sol_entrantes");
       console.log("result :>>", result);
     };
     getSol();
   }, []);
   
+*/
+
+import React, { useState, useEffect } from 'react';
+
+export default function Solicitudes() {
+  const [result, setResult] = useState();
+  const [blogs, setBlogs] = useState([]); 
+  
+  useEffect(()=>{
+    fetch('http://siscon.info/api/sol_entrantes', {
+      method: "GET",
+      headers: {"Authorization": `Bearer qFaUFzzBIF3M6uwN`}
+    }).then(res => res.json()).then(json => setResult(json));
+  },[]);
+
+  return (
+    <>
+      {JSON.stringify(result)}
+    </>
+  );
+};
